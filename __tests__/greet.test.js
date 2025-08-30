d3e189ed0bb1486cc71942c7643c21e93c4163c3
Mary Camille Rabang <117362412+marycamillerabang@users.noreply.github.com>
2025-08-30 12:07:54 +0800

    Update greet.test.js

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -1,5 +1,6 @@
+// __tests__/greet.test.js
 const { greet } = require('../src/greet');
 
 test('returns expected greeting', () => {
-  expect(greet('Team')).toBe('Hi, Camille! This is the updated Agile DevOps greeting. 8/2025');
+  expect(greet('Team')).toBe('Hi, Team! This is the updated Agile DevOps greeting. 8/2025');
 });
